1e0ef178f0353458833905ddc38b59e92f88119c
Tatianadardik44 <dardiktatiana@gmail.com>
2024-01-20 21:49:10 +0100

    one-dz

---
+++ js/js task-1.js
@@ -0,0 +1,11 @@
+function makeTransaction(quantity, pricePerDroid) {
+    const totalPrice = quantity * pricePerDroid;
+    let value = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    
+    return value;
+}
+makeTransaction();
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
+
