5c67a99091cf9bb500d51dc04da863a6b55d7f11
Tatianadardik44 <dardiktatiana@gmail.com>
2024-01-25 16:02:46 +0100

    dz-js-errors

--- js/task-2.js
+++ js/task-2.js
@@ -5,7 +5,7 @@ function getShippingMessage(country, price, deliveryFee)
     
     return sending;
 }
-getShippingMessage();
+
 console.log(getShippingMessage("Australia", 120, 50));
 console.log(getShippingMessage("Germany", 80, 20));
 console.log(getShippingMessage("Sweden", 100, 20));
--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,13 @@
 function getElementWidth(content, padding, border) {
-    const value = content + padding * 2 + border * 2;
-    console.log(value);
+    let contents = Number.parseFloat(content);
+    let paddings = Number.parseFloat(padding);
+    let borders = Number.parseFloat(border);
+
+    const value = contents + paddings * 2 + borders * 2;
+    
     return value;
- }   
-getElementWidth(50, 8, 4);
-getElementWidth(60, 12, 8.5);
-getElementWidth(200, 0, 0);
+}   
+ 
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
