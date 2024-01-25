45fe0ed428103cf6be9e23301eea081eec2a30c5
Tatianadardik44 <dardiktatiana@gmail.com>
2024-01-25 11:57:52 +0100

    js-hw-one

---
+++ js/js task-3.js
@@ -0,0 +1,11 @@
+function getElementWidth(content, padding, border) {
+    const value = content + padding * 2 + border * 2;
+    console.log(value);
+    return value;
+    
+   
+    
+}
+getElementWidth(50, 8, 4);
+getElementWidth(60, 12, 8.5);
+getElementWidth(200, 0, 0);
